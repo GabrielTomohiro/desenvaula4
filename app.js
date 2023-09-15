969957c3732a12a02d79e8b38ea2e37dd7add7e6
GabrielTomohiro <gabtom2005@gmail.com>
2023-09-14 21:40:35 -0300

    commit

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ app.get("/", (req, res) => {
 
 app.post("/cadastrar",function(req,res){
     post.create({
-        nome: req.body.nome
+        nome:req.body.nome,
+        endereco:req.body.endereco,
+        bairro:req.body.bairro, 
+        cep:req.body.cep,
+        cidade:req.body.cidade, 
+        estado:req.body.estado, 
+        observacao:req.body.observacao
     }).then(function(){
         console.log("dados caddastrados com sucesso!")
         res.send("Cadastro realizado com sucesso!")
@@ -45,4 +51,44 @@ app.get("/contato", function(req,res){
     res.send("contatino")
 })
 
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(post){
+        res.render("consulta", {post})
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
 
+app.get("/excluir/:id", function(req, res){
+    post.destroy({where: {'id': req.params.id}}.then(function(){
+        res.render("primeira_pagina")
+    }).catch(function(erro){
+        console.log("Erro ao excluir ou encontrar os dados do banco: " + erro)
+    }))
+})
+
+app.get("/editar/:id", function(req, res){
+    post.findAll({where: {'id': req.params.id}}).then(function(post){
+        res.render("editar", {post})
+    }).catch(function(erro){
+        console.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
+
+app.post("atualizar", function(req, res){
+    post.update({
+        nome:req.body.nome,
+        endereco:req.body.endereco,
+        bairro:req.body.bairro, 
+        cep:req.body.cep,
+        cidade:req.body.cidade, 
+        estado:req.body.estado, 
+        observacao:req.body.observacao
+    },{
+        where: {
+            id: req.body.id
+        }
+    }).then(function(){
+        res.redirect("/consulta")
+    })
+})
